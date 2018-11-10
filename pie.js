26852018876314a7f61ac7766942b7365435efb5
DenisSchulz <denis.schulzdjd@gmail.com>
2018-11-10 18:12:15 +0100

    Update pie.js

--- pie.js
+++ pie.js
@@ -14,7 +14,7 @@ function Pie()
     for(var i = 0; i < countUnique(3); i++)
     {
       arc(this.x,this.y,this.w,this.h,(i * 360 / countUnique(3)) + this.angle,((i + 1) * 360 / countUnique(3)) + this.angle,PIE);
-      console.log(contains(3,i));
+      //console.log(contains(3,i));
       for(var j = 0; j < contains(3,i).length; j++)
       {
         arc(this.x,this.y,this.w,this.h,(j * 360 / countUnique(2)) + this.angle,((j + 1) * 360 / countUnique(2)) + this.angle,PIE);
