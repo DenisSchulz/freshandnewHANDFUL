2c0732715a56eaee580fe5989fa1c37cfdbad2dc
DenisSchulz <denis.schulzdjd@gmail.com>
2018-11-10 16:43:11 +0100

    start of frontend

--- pie.js
+++ pie.js
@@ -1,13 +1,20 @@
-function Pie(x,y,s,start,part,array)
+function Pie()
 {
-  this.x = x;
-  this.y = y;
-  this.size = s;
-  this.start = start;
-  this.part = 360 / part;
-  this.array = array;
+  this.x = 200;
+  this.y = 200;
+  this.w = 300;
+  this.h = 300;
+  this.angle = -90;
 
-  this.pieces = [];
+  this.layers = [];
+
+  this.draw = function()
+  {
+    for(var i = 0; i < countUnique(3); i++)
+    {
+      arc(this.x, this.y, this.w * 0.25, this.h * 0.25, angle + i * 360 / countUnique(3), angle + (i + 1) * 360 / countUnique(3), PIE);
+    }
+  }
 
 
 
